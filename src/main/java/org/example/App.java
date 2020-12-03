a430813d3d5dd919f91a7bdba7e1d4b17853f78b
KySo4oK <prostorostuk08@gmail.com>
2020-12-03 23:17:40 +0200

    create new class for reading csv

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,43 +1,13 @@
 package org.example;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class App {
-    static List<List<String>> data = new ArrayList<>();
 
     public static void main(String[] args) {
-        loadFromFile("imdb.csv");
-    }
+        CSVParser parser = new CSVParser();
 
-    public static void loadFromFile(String fileName) {
-        try (FileReader reader = new FileReader(fileName)) {
-            Scanner scan = new Scanner(reader);
-            ArrayList<String> lines = new ArrayList<>();
-            scan.nextLine();
-            while (scan.hasNextLine()) {
-                lines.add(scan.nextLine());
-            }
-            data.addAll(lines.stream()
-                    .map(line -> Arrays.asList(line.split(",")))
-                    .collect(Collectors.toList()));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Error");
-        }
-    }
-
-    public static void printArr(String[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; i < arr[0].length; i++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        List<List<String>> imdb = parser.loadFromFile("imdb.csv");
+        List<List<String>> kinopoisk = parser.loadFromFile("kinopoisk.csv");
     }
 }
---
+++ src/main/java/org/example/CSVParser.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class CSVParser {
+    public List<List<String>> loadFromFile(String fileName) {
+        try (FileReader reader = new FileReader(fileName)) {
+            Scanner scan = new Scanner(reader);
+            ArrayList<String> lines = new ArrayList<>();
+            scan.nextLine();
+            while (scan.hasNextLine()) {
+                lines.add(scan.nextLine());
+            }
+            return lines.stream()
+                    .map(line -> Arrays.asList(line.split(",")))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("failed in - " + fileName + " with  -" + e.getMessage());
+        }
+    }
+}
