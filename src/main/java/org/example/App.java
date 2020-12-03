0fde15818cc9d323d1c16a8aa10198dff66dd1e7
KySo4oK <prostorostuk08@gmail.com>
2020-12-03 23:34:02 +0200

    add converting data to map

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.List;
+import java.util.Map;
 
 public class App {
 
@@ -9,5 +10,12 @@ public class App {
 
         List<List<String>> imdb = parser.loadFromFile("imdb.csv");
         List<List<String>> kinopoisk = parser.loadFromFile("kinopoisk.csv");
+
+        List<String> imdbHeader = imdb.remove(0);
+        List<String> kinopoiskHeader = kinopoisk.remove(0);
+
+        DataMerger merger = new DataMerger();
+        Map<String, List<String>> imdbMap = merger.convertDataToMap(imdb, 1);
+        Map<String, List<String>> kinopoiskMap = merger.convertDataToMap(imdb, 1);
     }
 }
--- src/main/java/org/example/DataMerger.java
+++ src/main/java/org/example/DataMerger.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DataMerger {
+    public Map<String, List<String>> convertDataToMap(List<List<String>> dataLines, int positionForKey) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (List<String> dataLine : dataLines) {
+            map.put(dataLine.remove(positionForKey), dataLine);
+        }
+        return map;
+    }
 }
