178b18d4c8e96a1f060650fbddd87c2492c34b87
KySo4oK <prostorostuk08@gmail.com>
2020-12-04 00:11:54 +0200

    add simple and not tested write to db

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,8 +8,8 @@ public class App {
     public static void main(String[] args) {
         CSVParser parser = new CSVParser();
 
-        List<List<String>> imdb = parser.loadFromFile("imdb.csv");
-        List<List<String>> kinopoisk = parser.loadFromFile("kinopoisk.csv");
+        List<List<String>> imdb = parser.loadFromFile("src/csv-tables/imdb.csv");
+        List<List<String>> kinopoisk = parser.loadFromFile("src/csv-tables/kinopoisk.csv");
 
         List<String> imdbHeader = imdb.remove(0);
         List<String> kinopoiskHeader = kinopoisk.remove(0);
@@ -19,5 +19,7 @@ public class App {
         Map<String, List<String>> kinopoiskMap = merger.convertDataToMap(imdb, 1);
 
         List<List<String>> result = merger.mergeTwoMaps(imdbMap, kinopoiskMap);
+        DBWriter dbWriter = new DBWriter();
+        dbWriter.write(result);
     }
 }
--- src/main/java/org/example/DBWriter.java
+++ src/main/java/org/example/DBWriter.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.sql.*;
+import java.util.List;
+
 public class DBWriter {
+    public void write(List<List<String>> result) {
+        for (List<String> line : result) {
+            writeOneLine(line);
+        }
+    }
+
+    private void writeOneLine(List<String> oneLine) {
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                        "postgres", "root")) {
+            try (PreparedStatement statement = connection.prepareStatement("")) {
+                for (int i = 0, oneLineSize = oneLine.size(); i < oneLineSize; i++) {
+                    String singleData = oneLine.get(i);
+                    statement.setString(i, singleData);
+                }
+                statement.execute();
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+    }
+
 }
