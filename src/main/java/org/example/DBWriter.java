64ae60a9b3e7371a20fcaca7a75bc23b4cbb2349
KySo4oK <prostorostuk08@gmail.com>
2020-12-05 14:03:39 +0200

    rewrite cycle with batch

--- src/main/java/org/example/DBWriter.java
+++ src/main/java/org/example/DBWriter.java
@@ -4,27 +4,30 @@ import java.sql.*;
 import java.util.List;
 
 public class DBWriter {
-    public void write(List<List<String>> result) {
-        for (List<String> line : result) {
-            writeOneLine(line);
-        }
-    }
 
-    private void writeOneLine(List<String> oneLine) {
+    public static final String query = "insert into combined_movies (rating, movie, year_eng, country, rating_eng, overview, director, screenwriter, actors,\n" +
+            "                             url_logo, rank, title, year_rus, linkmeta, rating_rus, duration, genre, metadate,\n" +
+            "                             summarytext, creditsinfo)\n" +
+            "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+    public void write(List<List<String>> result) {
         try (Connection connection = DriverManager
                 .getConnection("jdbc:postgresql://localhost:5432/postgres",
                         "postgres", "root")) {
-            try (PreparedStatement statement = connection.prepareStatement("")) {
-                for (int i = 0, oneLineSize = oneLine.size(); i < oneLineSize; i++) {
-                    String singleData = oneLine.get(i);
-                    statement.setString(i, singleData);
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                connection.setAutoCommit(false);
+                for (List<String> line : result) {
+                    for (int i = 0, oneLineSize = line.size(); i < oneLineSize; i++) {
+                        String singleData = line.get(i);
+                        statement.setString(i, singleData);
+                        statement.addBatch();
+                    }
+                    statement.executeBatch();
                 }
-                statement.execute();
             }
         } catch (SQLException e) {
             System.out.println("Connection failure.");
             e.printStackTrace();
         }
     }
-
 }
