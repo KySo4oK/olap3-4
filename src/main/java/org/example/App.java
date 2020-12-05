5eadd676d47df9eb65fbffb094c59fa9e49b4ac7
KySo4oK <prostorostuk08@gmail.com>
2020-12-05 13:38:38 +0200

    fix split with changing separator

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,15 +8,15 @@ public class App {
     public static void main(String[] args) {
         CSVParser parser = new CSVParser();
 
-        List<List<String>> imdb = parser.loadFromFile("src/csv-tables/imdb.csv", "\\\\s+,");
+        List<List<String>> imdb = parser.loadFromFile("src/csv-tables/imdb.csv", ";");
         List<List<String>> kinopoisk = parser.loadFromFile("src/csv-tables/kinopoisk.csv", ",");
 
-        List<String> imdbHeader = imdb.remove(0);
-        List<String> kinopoiskHeader = kinopoisk.remove(0);
+//        List<String> imdbHeader = imdb.remove(0);
+//        List<String> kinopoiskHeader = kinopoisk.remove(0);
 
         DataMerger merger = new DataMerger();
         Map<String, List<String>> imdbMap = merger.convertDataToMap(imdb, 1);
-        Map<String, List<String>> kinopoiskMap = merger.convertDataToMap(imdb, 1);
+        Map<String, List<String>> kinopoiskMap = merger.convertDataToMap(kinopoisk, 1);
 
         List<List<String>> result = merger.mergeTwoMaps(imdbMap, kinopoiskMap);
         DBWriter dbWriter = new DBWriter();
